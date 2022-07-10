b5e6bddb51685bf3366033064aee367232e17352
Phillip-England <phillip.mark.england@gmail.com>
2022-07-09 19:06:03 -0500

    working on individual expense items

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -4,42 +4,52 @@ import styles from './Expense.module.css'
 
 export default function Expense({
     expense,
+    activateExpense
 }){
 
     return(
-        <div className={styles.wrapper}>
-            <div className={styles.show}>
-                <h2 className={styles.date}>{expense.date}</h2>
-                <p className={styles.vendor}>{expense.vendor}</p>
-                <p className={styles.cost}>${expense.cost}</p>
-                <i className={`fa-solid fa-caret-up ${styles.icon}`}></i>
+        <div className={styles.wrapper} onClick={() => {activateExpense(expense.id)}}>
+            <div className={expense.active ? styles.showActive : styles.show}>
+                <h2 className={ styles.date}>{expense.date}</h2>
+                <p className={expense.active ? styles.vendorActive : styles.vendor}>{expense.vendor}</p>
+                <p className={expense.active ? styles.costActive : styles.cost}>${expense.cost}</p>
+                {expense.active
+                    ? <i className={`fa-solid fa-caret-down ${styles.iconActive}`}></i>
+                    : <i className={`fa-solid fa-caret-up ${styles.icon}`}></i>
+                }
             </div>
-            <div className={styles.hidden}>
-                <div className={styles.row}>
-                    <p className={styles.detailName}>Invoice</p>
-                    <p className={styles.detail}>{expense.invoice}</p>
+
+            {expense.active
+            ? 
+                <div className={styles.hidden}>
+                    <div className={styles.row}>
+                        <p className={styles.detailName}>Invoice</p>
+                        <p className={styles.detail}>{expense.invoice}</p>
+                    </div>
+                    <div className={styles.row}>
+                        <p className={styles.detailName}>Date</p>
+                        <p className={styles.detail}>{expense.date}</p>
+                    </div>
+                    <div className={styles.row}>
+                        <p className={styles.detailName}>Vendor</p>
+                        <p className={styles.detail}>{expense.vendor}</p>
+                    </div>
+                    <div className={styles.row}>
+                        <p className={styles.detailName}>Category</p>
+                        <p className={styles.detail}>{expense.category}</p>
+                    </div>
+                    <div className={styles.row}>
+                        <p className={styles.detailName}>Description</p>
+                        <p className={styles.detail}>{expense.description}</p>
+                    </div>
+                    <div className={styles.row}>
+                        <p className={styles.detailName}>Cost</p>
+                        <p className={styles.detail}>${expense.cost}</p>
+                    </div>
                 </div>
-                <div className={styles.row}>
-                    <p className={styles.detailName}>Date</p>
-                    <p className={styles.detail}>{expense.date}</p>
-                </div>
-                <div className={styles.row}>
-                    <p className={styles.detailName}>Vendor</p>
-                    <p className={styles.detail}>{expense.vendor}</p>
-                </div>
-                <div className={styles.row}>
-                    <p className={styles.detailName}>Category</p>
-                    <p className={styles.detail}>{expense.category}</p>
-                </div>
-                <div className={styles.row}>
-                    <p className={styles.detailName}>Description</p>
-                    <p className={styles.detail}>{expense.description}</p>
-                </div>
-                <div className={styles.row}>
-                    <p className={styles.detailName}>Cost</p>
-                    <p className={styles.detail}>${expense.cost}</p>
-                </div>
-            </div>
+            : null}
+
+
 
         </div>
     )
--- src/components/ExpenseList/ExpenseList.js
+++ src/components/ExpenseList/ExpenseList.js
@@ -3,31 +3,50 @@ import styles from './ExpenseList.module.css'
 import getAllProperties from '../../service/array/getAllProperties'
 import { useState } from "react"
 
-
-
-let expenseData = [
-    {id: 1, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Kitchen Supplies', description: 'Gym membership for David', cost: 250, isComplete: false},
-    {id: 2, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Repairs', description: 'Phone bill', cost: 250, isComplete: false},
-    {id: 3, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Repairs', description: 'Balloon man Neil', cost: 250, isComplete: false},
-    {id: 4, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Cleaning Supplies', description: 'Party Supplies', cost: 250, isComplete: false},
-    {id: 0, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Kitchen Supplies', description: 'A storage container', cost: 250, isComplete: false},
-    {id: 5, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Cleaning Supplies', description: 'Wreches, bolts, nuts, and a screwdriver. Also bought some more stuff', cost: 250, isComplete: false},
-    {id: 6, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Other Team Member Benefits', description: 'Party Supplies', cost: 250, isComplete: false},
-    {id: 7, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Other Team Member Benefits', description: 'Party Supplies', cost: 250, isComplete: false},
-    {id: 8, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Office Supplies', description: 'Party Supplies', cost: 250, isComplete: false},
-    {id: 9, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Office Supplies', description: 'Party Supplies', cost: 250, isComplete: false},
-]
-
-function getAllByProperty(array, propertyName, propertyValue){
-    return array.filter((item) => 
-        item[propertyName] === propertyValue
-    )
-}
-
 export default function ExpenseList() {
 
-    const [expenses, setExpenses] = useState(expenseData)
+    let expenseData = [
+        {id: 0, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Kitchen Supplies', description: 'Gym membership for David', cost: 250, isComplete: false},
+        {id: 1, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Repairs', description: 'Phone bill', cost: 250, isComplete: false},
+        {id: 2, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Repairs', description: 'Balloon man Neil', cost: 250, isComplete: false},
+        {id: 3, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Cleaning Supplies', description: 'Party Supplies', cost: 250, isComplete: false},
+        {id: 4, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Kitchen Supplies', description: 'A storage container', cost: 250, isComplete: false},
+        {id: 5, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Cleaning Supplies', description: 'Wreches, bolts, nuts, and a screwdriver. Also bought some more stuff', cost: 250, isComplete: false},
+        {id: 6, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Other Team Member Benefits', description: 'Party Supplies', cost: 250, isComplete: false},
+        {id: 7, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Other Team Member Benefits', description: 'Party Supplies', cost: 250, isComplete: false},
+        {id: 8, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Office Supplies', description: 'Party Supplies', cost: 250, isComplete: false},
+        {id: 9, invoice: 'June #1 2022', date:'01/01/2022', vendor: 'WalMart', category: 'Office Supplies', description: 'Party Supplies', cost: 250, isComplete: false},
+    ]
+
+    function getAllByProperty(array, propertyName, propertyValue){
+        return array.filter((item) => 
+            item[propertyName] === propertyValue
+        )
+    }
+
+    function addActiveAndAnimate(arrayOfObjects){
+        let copyOfArray = Object.assign([], arrayOfObjects)
+        for (let x = 0; x < copyOfArray.length; x++){
+            copyOfArray[x].animate = null
+            copyOfArray[x].active = false
+        }
+        return copyOfArray
+    }
+
+    const [expenses, setExpenses] = useState(addActiveAndAnimate(expenseData))
     const [categories, setCategories] = useState(getAllProperties(expenses, 'category'))
+
+    function activateExpense(key) {
+        let copyOfExpenses = Object.assign([], expenses)
+        copyOfExpenses.forEach(expense => {
+            if (expense.id === key){
+                expense.active = !expense.active
+            } else {
+                expense.active = false
+            }
+        })
+        setExpenses(copyOfExpenses)
+    }
 
     return(
         categories.map((category) => {
@@ -38,6 +57,7 @@ export default function ExpenseList() {
                         <Expense
                             expense={expense}
                             key={expense.id}
+                            activateExpense={activateExpense}
                         /> 
                     )}
                 </div>
